85e305ef1e8482a7b6cadf8a678abb3f6baec3d1
ShaiNachmaniExacti <108803047+ShaiNachmaniExacti@users.noreply.github.com>
2025-05-18 11:32:56 +0300

    add dokerfile

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import uvicorn
 from src.core.process_every_action_data import app
+import os
 
 # This file just imports and uses the FastAPI app from process_every_action_data.py
 # Run with: python -m uvicorn main:app --host 0.0.0.0 --port 8000 --reload 
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port) 
